760da7c8b6fc1e6464a42295020592d3d85365d2
obaidaxt <obaidaxt@gmail.com>
2023-11-10 09:50:33 +0100

    1011

--- lib/main.dart
+++ lib/main.dart
@@ -23,137 +23,214 @@ class FacebookApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("facebook",
-              style: TextStyle(
-                  color: Colors.blueAccent,
-                  fontSize: 27,
-                  fontWeight: FontWeight.w700)),
-          centerTitle: true,
-          leading: IconButton(
+      appBar: AppBar(
+        title: Text("facebook",
+            style: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 30,
+                fontWeight: FontWeight.w700)),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.blueAccent,
+            size: 30,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.message,
+                color: Colors.blueAccent,
+                size: 30,
+              ),
+              onPressed: () {}),
+          IconButton(
             icon: Icon(
-              Icons.menu,
+              Icons.search,
               color: Colors.blueAccent,
               size: 30,
             ),
             onPressed: () {},
           ),
-          actions: [
-            IconButton(
-                icon: Icon(
-                  Icons.message,
-                  color: Colors.blueAccent,
-                  size: 30,
-                ),
-                onPressed: () {}),
-            IconButton(
-              icon: Icon(
-                Icons.search,
-                color: Colors.blueAccent,
-                size: 30,
-              ),
-              onPressed: () {},
-            ),
-          ],
-          backgroundColor: Colors.white,
-          elevation: 20,
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.only(bottom: 22),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.blueGrey,
-                ),
-                height: 322,
-                width: double.infinity,
-                child: Text(
-                  "Hello",
-                  style: TextStyle(color: Colors.white, fontSize: 44),
-                ),
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
+        ],
+        backgroundColor: Colors.white,
+        elevation: 20,
+      ),
+      body: Container(
+        color: Colors.blueGrey,
+        height: 500,
+        width: double.infinity,
+        child: Column(
+          children: [
+            Container(
+              color: Colors.blue[200],
+              width: 250,
+              height: 320,
+              child: Center(
+                child: Wrap(
+                  direction: Axis.vertical,
+                  alignment: WrapAlignment.end,
+                  spacing: 10,
+                  runSpacing: 11,
                   children: [
-                    Container(
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.blueGrey,
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                        ),
+                        padding: MaterialStateProperty.all(
+                          EdgeInsets.all(27),
+                        ),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.orange[500]),
                       ),
-                      height: 100,
-                      width: 150,
                       child: Text(
-                        "Welcome",
-                        style: TextStyle(color: Colors.white, fontSize: 27),
+                        '1',
+                        style: TextStyle(fontSize: 27),
                       ),
                     ),
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 23),
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.blueGrey,
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                        ),
+                        padding: MaterialStateProperty.all(
+                          EdgeInsets.all(27),
+                        ),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.orange[500]),
                       ),
-                      height: 100,
-                      width: 150,
                       child: Text(
-                        "Noah",
-                        style: TextStyle(color: Colors.white, fontSize: 27),
+                        '2',
+                        style: TextStyle(fontSize: 27),
                       ),
                     ),
-                    Container(
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.blueGrey,
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                        ),
+                        padding: MaterialStateProperty.all(
+                          EdgeInsets.all(27),
+                        ),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.orange[500]),
                       ),
-                      height: 100,
-                      width: 150,
                       child: Text(
-                        "Hi",
-                        style: TextStyle(color: Colors.white, fontSize: 27),
+                        '3',
+                        style: TextStyle(fontSize: 27),
                       ),
                     ),
-                    Container(
-                      margin: EdgeInsets.only(left: 11),
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.blueGrey,
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                        ),
+                        padding: MaterialStateProperty.all(
+                          EdgeInsets.all(27),
+                        ),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.orange[500]),
                       ),
-                      height: 100,
-                      width: 150,
                       child: Text(
-                        "Obaida",
-                        style: TextStyle(color: Colors.white, fontSize: 27),
+                        '4',
+                        style: TextStyle(fontSize: 27),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                        ),
+                        padding: MaterialStateProperty.all(
+                          EdgeInsets.all(27),
+                        ),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.orange[500]),
+                      ),
+                      child: Text(
+                        '5',
+                        style: TextStyle(fontSize: 27),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                        ),
+                        padding: MaterialStateProperty.all(
+                          EdgeInsets.all(27),
+                        ),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.orange[500]),
+                      ),
+                      child: Text(
+                        '6',
+                        style: TextStyle(fontSize: 27),
                       ),
                     ),
                   ],
                 ),
               ),
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.blueGrey,
-                ),
-                height: 322,
-                width: double.infinity,
-                child: Text(
-                  "Noah...",
-                  style: TextStyle(color: Colors.white, fontSize: 44),
-                ),
-              )
-            ],
-          ),
-        ));
+            ),
+            // Expanded(
+            //   flex: 1,
+            //   child: Container(
+            //     alignment: Alignment.center,
+            //     color: Colors.amber[200],
+            //     width: 99,
+            //     height: 100,
+            //     child: Text(
+            //       "Noah",
+            //       style: TextStyle(
+            //         fontSize: 19,
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            // Expanded(
+            //   flex: 2,
+            //   child: Container(
+            //     alignment: Alignment.center,
+            //     color: Colors.blue[300],
+            //     width: 99,
+            //     height: 100,
+            //     child: Text(
+            //       "Obaida",
+            //       style: TextStyle(fontSize: 19, color: Colors.white),
+            //     ),
+            //   ),
+            // ),
+            // Expanded(
+            //   flex: 1,
+            //   child: Container(
+            //     alignment: Alignment.center,
+            //     color: Colors.red[300],
+            //     width: 99,
+            //     height: 100,
+            //     child: Text(
+            //       "Hi",
+            //       style: TextStyle(fontSize: 19, color: Colors.white),
+            //     ),
+            //   ),
+            // )
+          ],
+        ),
+      ),
+    );
   }
 }
-// mainAxisAlignment → MainAxisAlignment.
-// crossAxisAlignment → CrossAxisAlignment.
